3c22abb916a6ae654fc71e2a1e05f1ec0f548ce1
savary1 <david.savarymartinez@gmail.com>
2019-05-03 21:27:40 +0200

    Creados todos los fragments de la main activity.
    Implementado el cambio de un fragment a otro.

--- app/src/main/java/fdi/pad/ucmbooks/MainActivity.java
+++ app/src/main/java/fdi/pad/ucmbooks/MainActivity.java
@@ -2,40 +2,15 @@ package fdi.pad.ucmbooks;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_libreria:
-                    mTextMessage.setText(R.string.title_libreria);
-                    return true;
-                case R.id.navigation_buscar:
-                    mTextMessage.setText(R.string.title_buscar);
-                    return true;
-                case R.id.navigation_leidos:
-                    mTextMessage.setText(R.string.title_leidos);
-                    return true;
-            }
-            return false;
-        }
-    };
-
-    @Override
-    public boolean onCreateOptionsMenu(android.view.Menu menu) {
-        // Inflate the menu, this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.app_menu, menu);
-        return true;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +19,49 @@ public class MainActivity extends AppCompatActivity {
         android.support.v7.widget.Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navView.setOnNavigationItemSelectedListener(this);
+        LibreriaFragment firstFragment = new LibreriaFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        // Inflate the menu, this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+    private boolean switchFragment(android.support.v4.app.Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_libreria:
+                fragment = new LibreriaFragment();
+                break;
+
+            case R.id.navigation_buscar:
+                fragment = new BuscarFragment();
+                break;
+
+            case R.id.navigation_leidos:
+                fragment = new LeidosFragment();
+                break;
+        }
+
+        return switchFragment(fragment);
+    }
 }
