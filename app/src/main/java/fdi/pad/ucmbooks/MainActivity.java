6250a42b714b032212c5029de98b700dccc08295
savary1 <david.savarymartinez@gmail.com>
2019-05-03 13:15:34 +0200

    Añadida toolbar con nombre de la app y menu en la parte superior de la pantalla principal

--- app/src/main/java/fdi/pad/ucmbooks/MainActivity.java
+++ app/src/main/java/fdi/pad/ucmbooks/MainActivity.java
@@ -31,9 +31,18 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        // Inflate the menu, this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.bottom_nav_menu, menu);
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.support.v7.widget.Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
