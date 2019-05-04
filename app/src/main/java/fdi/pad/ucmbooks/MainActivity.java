15ca60588384353ecacf30b625d846813125bfed
savary1 <david.savarymartinez@gmail.com>
2019-05-04 21:45:43 +0200

    Añadida barra de busqueda en la toolbar cuando esta seleccionado el fragmento buscar

--- app/src/main/java/fdi/pad/ucmbooks/MainActivity.java
+++ app/src/main/java/fdi/pad/ucmbooks/MainActivity.java
@@ -5,21 +5,34 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.MenuInflater;
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v7.widget.SearchView;
+
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
+    private Toolbar myToolbar;
     private LibreriaFragment libreria = new LibreriaFragment();
     private BuscarFragment buscar = new BuscarFragment();
     private LeidosFragment leidos = new LeidosFragment();
+
+    private enum FR_TYPE{
+        LIBRERIA, BUSCAR, LEIDOS
+    }
+
+    FR_TYPE currentFragment = FR_TYPE.LIBRERIA;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        android.support.v7.widget.Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(this);
@@ -29,7 +42,24 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     public boolean onCreateOptionsMenu(android.view.Menu menu) {
         // Inflate the menu, this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.app_menu, menu);
+
+        switch(currentFragment) {
+            case BUSCAR:{
+                MenuInflater inflater = getMenuInflater();
+                inflater.inflate(R.menu.search_menu, menu);
+
+                // Get the SearchView and set the searchable configuration
+                SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+                SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+                // Assumes current activity is the searchable activity
+                searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+                searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+                break;
+            }
+            default:
+                getMenuInflater().inflate(R.menu.app_menu, menu);
+        }
+
         return true;
     }
 
@@ -52,16 +82,21 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         switch (menuItem.getItemId()) {
             case R.id.navigation_libreria:
                 fragment = this.libreria;
+                currentFragment = FR_TYPE.LIBRERIA;
                 break;
 
             case R.id.navigation_buscar:
                 fragment = this.buscar;
+                currentFragment = FR_TYPE.BUSCAR;
                 break;
 
             case R.id.navigation_leidos:
                 fragment = this.leidos;
+                currentFragment = FR_TYPE.LEIDOS;
                 break;
         }
+
+        invalidateOptionsMenu();
 
         return switchFragment(fragment);
     }
